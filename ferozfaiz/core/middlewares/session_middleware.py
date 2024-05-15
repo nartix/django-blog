01ba3b2efca164a2b2741d2a06a5a380b8b678dc
feroz <nartix@gmail.com>
2024-05-15 10:27:25 +0000

    used cf_connecting_ip to get real ip

--- ferozfaiz/core/middlewares/session_middleware.py
+++ ferozfaiz/core/middlewares/session_middleware.py
@@ -1,4 +1,8 @@
 from django.utils.deprecation import MiddlewareMixin
+import logging
+
+logger = logging.getLogger("core")
+
 
 class SessionMiddleware(MiddlewareMixin):
     def process_request(self, request):
@@ -18,9 +22,20 @@ class SessionMiddleware(MiddlewareMixin):
 
     @staticmethod
     def get_client_ip(request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+        cf_connecting_ip = request.META.get('HTTP_CF_CONNECTING_IP')
+
+        # user ip
+        # logger.info(
+        #     f'CF_CONNECTING_IP : {request.META.get("HTTP_CF_CONNECTING_IP")}')
+        # load balancer ip
+        # logger.info(f'x_forwarded_for: {request.META.get('HTTP_X_FORWARDED_FOR')}')
+        # load balancer ip
+        # logger.info(f'HTTP_X_REAL_IP: {request.META.get("HTTP_X_REAL_IP")}')
+        # 10.42.0.100 node ip
+        # logger.info(f'REMOTE_ADDR: {request.META.get("REMOTE_ADDR")}')
+
+        if cf_connecting_ip:
+            ip = cf_connecting_ip.split(',')[0]
         else:
             ip = request.META.get('REMOTE_ADDR')
         return ip
